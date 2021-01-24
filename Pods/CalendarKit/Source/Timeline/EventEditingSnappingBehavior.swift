37fc200d6c7aa4d96618c7f900a99b8d1e88a3be
Vikram Singh <vik.devmail@gmail.com>
2021-01-24 12:43:08 -0500

    Remove all project files from gh-pages branch

--- Pods/CalendarKit/Source/Timeline/EventEditingSnappingBehavior.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-public protocol EventEditingSnappingBehavior {
-  init(_ calendar: Calendar)
-  func nearestDate(to date: Date) -> Date
-  func accentedHour(for date: Date) -> Int
-  func accentedMinute(for date: Date) -> Int
-}
