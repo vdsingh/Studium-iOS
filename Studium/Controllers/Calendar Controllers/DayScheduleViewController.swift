557acd2ae9ac34cd3568c8112d666d2082df1927
Vikram Singh <vik.devmail@gmail.com>
2023-04-28 23:59:13 -0400

    NC: DayScheduleVC changes: reimplement creating events

--- Studium/Controllers/Calendar Controllers/DayScheduleViewController.swift
+++ Studium/Controllers/Calendar Controllers/DayScheduleViewController.swift
@@ -12,7 +12,7 @@ import DateToolsSwift
 //
 class DayScheduleViewController: DayViewController {
     
-    let debug = false
+    let debug = true
     
     let defaults = UserDefaults.standard
     
@@ -82,179 +82,94 @@ class DayScheduleViewController: DayViewController {
         performSegue(withIdentifier: "toCalendar", sender: self)
     }
     
-    func addCourses(for date: Date) -> [Event]{
-        var events: [Event] = []
-        var coursesOnDay: [Course] = []
-        let allCourses = DatabaseService.shared.getStudiumObjects(expecting: Course.self)
-        for course in allCourses{
-            if course.days.contains(date.studiumWeekday){ //course occurs on this day.
-                coursesOnDay.append(course)
-            }
-        }
-        
-        for course in coursesOnDay{
-            let courseEvent = Event()
-            courseEvent.startDate = Calendar.current.date(bySettingHour: course.startDate.hour, minute: course.startDate.minute, second: 0, of: date)!
-            courseEvent.endDate = Calendar.current.date(bySettingHour: course.endDate.hour, minute: course.endDate.minute, second: 0, of: date)!
-            courseEvent.color = course.color
+    
+    //TODO: Docstring
+    func createEventsForStudiumEvents(on date: Date) -> [Event] {
+        printDebug("createEventsForStudiumEvents called")
+        let studiumEvents = DatabaseService.shared.getAllStudiumObjects()
+        var events = [Event]()
+        for studiumEvent in studiumEvents {
+            printDebug("Creating StudiumEvent: \(studiumEvent.name) for Date: \(date)")
             
-            let string = "\(course.startDate.format(with: "h:mm a")) - \(course.endDate.format(with: "h:mm a")): \(course.name)"
-            let attributes = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 13), NSAttributedString.Key.foregroundColor: UIColor.label]
-            let attributedString = NSMutableAttributedString(string: string, attributes: attributes)
-            courseEvent.attributedText = attributedString
-            events.append(courseEvent)
-        }
-        return events
-    }
-    
-    func addWakeTimes(for date: Date) -> [Event]{
-        var events: [Event] = []
-        
-        if UserDefaults.standard.object(forKey: K.wakeUpKeyDict[date.weekday]!) == nil {
-            print("$Log: user did not specify wake times.")
-            return []
-        }
-        
-        let timeToWake = defaults.array(forKey: K.wakeUpKeyDict[date.weekday]!)![0] as! Date
-        
-        let hour = calendar.component(.hour, from: timeToWake)
-        let minutes = calendar.component(.minute, from: timeToWake)
-        let usableDate = Calendar.current.date(bySettingHour: hour, minute: minutes, second: 0, of: date)!
-
-        let anHourAgo = usableDate - (60 * 60)
-        let newEvent = Event()
-        newEvent.startDate = anHourAgo
-        newEvent.endDate = usableDate
-        newEvent.color = UIColor.yellow
-        
-        
-        let string = "\(usableDate.format(with: DateFormat.standardTime.rawValue)): Wake Up"
-        
-        let attributes = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 13), NSAttributedString.Key.foregroundColor: UIColor.label]
-        let attributedString = NSMutableAttributedString(string: string, attributes: attributes)
-        newEvent.attributedText = attributedString
-        
-        events.append(newEvent)
-        return events
-    }
-    
-    func addHabits(for date: Date, with outsideEvents: [Event]) -> [Event]{//algorithm to find right time based on pre-existing events.
-        let allHabits = DatabaseService.shared.getStudiumObjects(expecting: Habit.self)
-//        let allHabits = realm.objects(Habit.self)
-        var events: [Event] = []
-        
-        var habitsOnDay: [Habit] = []
-        for habit in allHabits{
-            if habit.days.contains(date.studiumWeekday){ //course occurs on this day.
-                habitsOnDay.append(habit)
-            }
-        }
-        for habit in habitsOnDay{
-//            if habit.autoschedule{ // auto schedule habit
-                let dateFormatter = DateFormatter()
-                dateFormatter.dateFormat = "EEEE"
-                let weekDay = dateFormatter.string(from: date) //get weekday name. ex: "Tuesday"
-                //print(weekDay)
-//                let usableString = weekDay.substring(toIndex: 3)//transform it to a usable string. ex: "Tuesday" to "Tue"
-                if habit.days.contains(date.studiumWeekday) {
-                    //habit occurs on this day
-                    var components = Calendar.current.dateComponents([.hour, .minute], from: habit.startDate)
-                    var usableStartDate = Calendar.current.date(bySettingHour: components.hour!, minute: components.minute!, second: 0, of: date)!
+            let newEvent = Event()
+            newEvent.startDate = studiumEvent.startDate
+            newEvent.endDate = studiumEvent.endDate
+            newEvent.color = studiumEvent.scheduleDisplayColor
+            let attributes = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 13), NSAttributedString.Key.foregroundColor: StudiumColor.primaryLabel.uiColor]
+            let attributedString = NSMutableAttributedString(string: studiumEvent.scheduleDisplayString, attributes: attributes)
+            newEvent.attributedText = attributedString
+            
+            // Is the event recurring?
+            if let recurringEvent = studiumEvent as? RecurringStudiumEvent {
+                
+                if let recurringEvent = recurringEvent as? Autoscheduleable,
+                   recurringEvent.autoschedule {
+//                    let autoscheduleChunk = AutoscheduleService.shared.findAutoscheduleTimeChunk(dateToScheduleOn: date, startBound: <#T##Date#>, endBound: <#T##Date#>, totalMinutes: <#T##Int#>)
+                } else {
                     
-                    components = Calendar.current.dateComponents([.hour, .minute], from: habit.endDate)
-                    var usableEndDate = Calendar.current.date(bySettingHour: components.hour!, minute: components.minute!, second: 0, of: date)!
-                    
-                    //if this habit is autoscheduled, we use the Autoschedule algorithm from Autoschedule class to find the best time for this event to occur. We then change usableStartDate and usableEndDate to the dates calculated by the algorithm.
-                    if habit.autoschedule{
-                        let dates: [Date]? = Autoschedule.getStartAndEndDates(dateOccurring: date, startBound: usableStartDate, endBound: usableEndDate, totalMinutes: habit.autoLengthMinutes)
-                        //there were no open time slots.
-                        if dates == nil{
-                            continue
-                        }
-                        print("AUTOSCHEDULING HABIT: \(habit.name) with minutes: \(habit.autoLengthMinutes). AND FOUND DATES: \(dates ?? [])")
-                        usableStartDate = dates![0]
-                        print("Start: \(usableStartDate.format(with: "h:mm a zzz"))")
-                        
-                        usableEndDate = dates![1]
-
-//                        components = Calendar.current.dateComponents([.hour, .minute], from: dates[0])
-//                        usableStartDate = Calendar.current.date(bySettingHour: components.hour!, minute: components.minute!, second: 0, of: date)!
-//
-//                        components = Calendar.current.dateComponents([.hour, .minute], from: dates[1])
-//                        usableEndDate = Calendar.current.date(bySettingHour: components.hour!, minute: components.minute!, second: 0, of: date)!
+                    // Does the event occur on the requested date?
+                    if let timechunk = recurringEvent.timeChunkForDate(date: date) {
+                        newEvent.startDate = Calendar.current.date(bySettingHour: studiumEvent.startDate.hour, minute: studiumEvent.startDate.minute, second: 0, of: date)!
+                        newEvent.endDate = Calendar.current.date(bySettingHour: studiumEvent.endDate.hour, minute: studiumEvent.endDate.minute, second: 0, of: date)!
                     }
-                    let newEvent = Event()
-                    newEvent.startDate = usableStartDate
-                    newEvent.endDate = usableEndDate
-                    newEvent.text = "\(usableStartDate.format(with: DateFormat.standardTime.rawValue)) - \(usableEndDate.format(with: DateFormat.standardTime.rawValue)): \(habit.name)"
-                    events.append(newEvent)
                 }
+            } else {
+//                continue
             }
-        return events
-    }
-    
-    
-    
-    func addAssignments(for date: Date) -> [Event]{
-        var events: [Event] = []
-        let allAssignments = DatabaseService.shared.getStudiumObjects(expecting: Assignment.self)
-
-//        let allAssignments = realm.objects(Assignment.self)
-        for assignment in allAssignments {
-            if assignment.endDate.year == date.year && assignment.endDate.month == date.month && assignment.endDate.day == date.day{
-                guard let course = assignment.parentCourse else {
-                    print("$Error (DayScheduleViewController): Parent course was nil when adding assignments")
-                    continue
-                }
-                
-                let newEvent = Event()
-                newEvent.startDate = assignment.startDate
-                newEvent.endDate = assignment.endDate
-                newEvent.color = course.color
-                
-                var string = "\(assignment.endDate.format(with: "h:mm a")): \(assignment.name) due (\(course.name))"
-                
-                if(assignment.isAutoscheduled){
-                    string = "\(assignment.startDate.format(with: "h:mm a")) - \(assignment.endDate.format(with: "h:mm a")): \(assignment.name) (\(course.name))"
-                }
-
-                let attributes = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 13), NSAttributedString.Key.foregroundColor: UIColor.label]
-                let attributedString = NSMutableAttributedString(string: string, attributes: attributes)
-                if assignment.complete{
-                    print("Assignment \(assignment.name) is complete!");
+            
+            if let studiumEvent = studiumEvent as? CompletableStudiumEvent {
+                if studiumEvent.complete {
                     attributedString.addAttributes([
-                    NSAttributedString.Key.strikethroughStyle: NSUnderlineStyle.single.rawValue,
+                        NSAttributedString.Key.strikethroughStyle: NSUnderlineStyle.single.rawValue,
                         NSAttributedString.Key.strikethroughColor: UIColor.label,
                     ], range: NSMakeRange(0, attributedString.length))
                 }
-                newEvent.attributedText = attributedString
-                events.append(newEvent)
-                print("added assignment \(assignment.name) with start time \(newEvent.startDate) and end time \(newEvent.endDate)")
             }
+
+            printDebug("Appending event with text \(newEvent.attributedText?.string) and dates \(newEvent.startDate) - \(newEvent.endDate)")
+            events.append(newEvent)
         }
+        
+        
         return events
     }
     
-    func addOtherEvents(for date: Date) -> [Event]{
-        var events: [Event] = []
-        let allOtherEvents = DatabaseService.shared.getStudiumObjects(expecting: OtherEvent.self)
-
-        for otherEvent in allOtherEvents{
-            if otherEvent.endDate.year == date.year && otherEvent.endDate.month == date.month && otherEvent.endDate.day == date.day{
-                let newEvent = Event()
-                newEvent.startDate = otherEvent.startDate
-                newEvent.endDate = otherEvent.endDate
-                newEvent.color = .blue
-                let string = "\(otherEvent.startDate.format(with: "h:mm a")) - \(otherEvent.endDate.format(with: "h:mm a")) - \(otherEvent.name)"
-                
-                let attributes = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 13), NSAttributedString.Key.foregroundColor: UIColor.label]
-                let attributedString = NSMutableAttributedString(string: string, attributes: attributes)
-                newEvent.attributedText = attributedString
-                
-                events.append(newEvent)
-            }
+    func createWakeTimeEvent(for date: Date) -> Event? {
+        printDebug("Creating Wake Time Events")
+        
+        // There is no wake up time for the specified date
+        guard let wakeUpTime = DatabaseService.shared.getUserSettings().getWakeUpTime(for: date) else {
+            printDebug("No wake up time for \(date.studiumWeekday)")
+            return nil
         }
-        return events
+        
+//        if UserDefaults.standard.object(forKey: K.wakeUpKeyDict[date.weekday]!) == nil {
+//            printDebug("user did not specify wake times.")
+//            return []
+//        }
+        
+//        let timeToWake = defaults.array(forKey: K.wakeUpKeyDict[date.weekday]!)![0] as! Date
+        
+//        let hour = calendar.component(.hour, from: timeToWake)
+//        let minutes = calendar.component(.minute, from: timeToWake)
+//        let usableDate = Calendar.current.date(bySettingHour: hour, minute: minutes, second: 0, of: date)!
+
+        let anHourAgo = wakeUpTime - (60 * 60)
+        let newEvent = Event()
+        newEvent.startDate = anHourAgo
+        newEvent.endDate = wakeUpTime
+        newEvent.color = UIColor.yellow
+        
+        
+        let string = "\(wakeUpTime.format(with: DateFormat.standardTime.rawValue)): Wake Up"
+        
+        let attributes = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 13), NSAttributedString.Key.foregroundColor: StudiumColor.primaryLabel.uiColor]
+        let attributedString = NSMutableAttributedString(string: string, attributes: attributes)
+        newEvent.attributedText = attributedString
+        
+        printDebug("Created Wake Up Event for \(date). Wake Up Time is \(wakeUpTime)")
+
+        return newEvent
     }
     
     // MARK: EventDataSource
@@ -262,22 +177,29 @@ class DayScheduleViewController: DayViewController {
     var alreadyGeneratedSet = Set<Date>()
     
     override func eventsForDate(_ date: Date) -> [EventDescriptor] {
-        if !alreadyGeneratedSet.contains(date) {
-            alreadyGeneratedSet.insert(date)
-            
-            generatedEvents.append(contentsOf: generateEventsForDate(date))
-        }
-        return generatedEvents
+//        if !alreadyGeneratedSet.contains(date) {
+//            alreadyGeneratedSet.insert(date)
+//
+//            generatedEvents.append(contentsOf: generateEventsForDate(date))
+//        }
+//        return generatedEvents
+        printDebug("eventsForDate called")
+        return self.generateEventsForDate(date)
     }
     
-    private func generateEventsForDate(_ date: Date) -> [EventDescriptor] {
-        
+    func generateEventsForDate(_ date: Date) -> [EventDescriptor] {
+        printDebug("generateEventsForDate called")
         var events = [Event]()
-        events.append(contentsOf: addWakeTimes(for: date))
-        events.append(contentsOf: addCourses(for: date))
-        events.append(contentsOf: addOtherEvents(for: date))
-        events.append(contentsOf: addHabits(for: date, with: events))
-        events.append(contentsOf: addAssignments(for: date))
+        events.append(contentsOf: self.createEventsForStudiumEvents(on: date))
+        
+        if let event = self.createWakeTimeEvent(for: date) {
+            events.append(event)
+        }
+//        events.append(contentsOf: addWakeTimes(for: date))
+//        events.append(contentsOf: addCourses(for: date))
+//        events.append(contentsOf: addOtherEvents(for: date))
+//        events.append(contentsOf: addHabits(for: date, with: events))
+//        events.append(contentsOf: addAssignments(for: date))
     
         return events
     }
