89fe86af37606e1750e17f5ad2ad1f5c87cf6414
Vikram Singh <vik.devmail@gmail.com>
2023-05-06 23:53:26 -0400

    NC: Fix wake up times bug (use Realm Map)

--- Studium/Models/UserSettings.swift
+++ Studium/Models/UserSettings.swift
@@ -17,37 +17,27 @@ final class UserSettings: Object {
     @Persisted var _id = "UserSettings"
     
     //TODO: Docstrings
-    @Persisted private var wakeUpTimes: List<WakeUpTime>
+    @Persisted private var _wakeUpMap: Map<String, Date>
     
     //TODO: Docstrings
     @Persisted private var defaultAlertOptionRawValues: List<Int>
     
     //TODO: Docstrings
+    var wakeUpTimeMap: [Weekday: Date] {
+        var dict = [Weekday: Date]()
+        for wakeUpTime in self._wakeUpMap.keys {
+            if let intValue = wakeUpTime.parseToInt(),
+               let weekday = Weekday(rawValue: intValue) {
+                dict[weekday] = _wakeUpMap[wakeUpTime]
+            }
+        }
+
+        return dict
+    }
+    
+    //TODO: Docstrings
     var defaultAlertOptions: [AlertOption] {
         return self.defaultAlertOptionRawValues.compactMap({ AlertOption(rawValue: $0) })
-    }
-    
-    //TODO: Docstrings
-    convenience init(weekdayCases: [Weekday]) {
-        self.init()
-        
-        for weekday in weekdayCases {
-            let wakeUpTime = WakeUpTime(weekday: weekday, wakeUpTime: nil)
-            self.wakeUpTimes.append(wakeUpTime)
-        }
-    }
-    
-    //TODO: Docstrings
-    func getWakeUpTime(for date: Date) -> Date? {
-        return self.wakeUpTimes.first(where: { $0.weekday == date.studiumWeekday })?.wakeUpTime ?? nil
-    }
-    
-    //TODO: Docstrings
-    func setWakeUpTime(for weekday: Weekday, wakeUpTime: Date?) {
-        if let wakeUpTime = wakeUpTime,
-           let wakeUpObject = self.wakeUpTimes.first(where: { $0.weekday.rawValue == weekday.rawValue }) {
-            wakeUpObject.setWakeUpTime(wakeUpTime: wakeUpTime)
-        }
     }
     
     //TODO: Docstrings
@@ -63,50 +53,70 @@ final class UserSettings: Object {
     }
 }
 
-//TODO: Docstrings
-final class WakeUpTime: Object, Debuggable {
-    
+// MARK: - Wake Up Time
+extension UserSettings {
     //TODO: Docstrings
-    let debug = true
-    
-    //TODO: Docstrings
-    @Persisted var _id = ObjectId.generate()
-    
-    //TODO: Docstrings
-    @Persisted private var weekdayInt: Int
-    
-    //TODO: Docstrings
-    @Persisted var wakeUpTime: Date?
-    
-    //TODO: Docstrings
-    var weekday: Weekday {
-        get { return Weekday(rawValue: self.weekdayInt) ?? .unknown }
-        set { self.weekdayInt = newValue.rawValue }
+    func getWakeUpTime(for date: Date) -> Date? {
+        let time = self.getWakeUpTime(for: date.studiumWeekday)
+        return time?.setDate(year: date.year, month: date.month, day: date.day)
     }
     
     //TODO: Docstrings
-    convenience init(weekday: Weekday, wakeUpTime: Date?) {
-        self.init()
-        self.weekday = weekday
-        self.wakeUpTime = wakeUpTime
+    private func getWakeUpTime(for weekday: Weekday) -> Date? {
+        return self.wakeUpTimeMap[weekday]
     }
     
     //TODO: Docstrings
-    func setWakeUpTime(wakeUpTime: Date) {
-        self.wakeUpTime = wakeUpTime
-        self.printDebug("Set wake up time for weekday value \(self.weekdayInt) to \(self.wakeUpTime)")
-    }
-    
-    override static func primaryKey() -> String? {
-         return "_id"
-    }
-    
-    func printDebug(_ message: String) {
-        if self.debug {
-            print("$LOG (WakeUpTime): \(message)")
-        }
+    func setWakeUpTime(for weekday: Weekday, wakeUpTime: Date) {
+        let stringValue = "\(weekday.rawValue)"
+        self._wakeUpMap[stringValue] = wakeUpTime
     }
 }
+
+//TODO: Docstrings
+//final class WakeUpTime: Object, Debuggable {
+//
+//    //TODO: Docstrings
+//    let debug = true
+//
+//    //TODO: Docstrings
+//    @Persisted var _id = ObjectId.generate()
+//
+//    //TODO: Docstrings
+//    @Persisted private var weekdayInt: Int
+//
+//    //TODO: Docstrings
+//    @Persisted var wakeUpTime: Date?
+//
+//    //TODO: Docstrings
+//    var weekday: Weekday {
+//        get { return Weekday(rawValue: self.weekdayInt) ?? .unknown }
+//        set { self.weekdayInt = newValue.rawValue }
+//    }
+//
+//    //TODO: Docstrings
+//    convenience init(weekday: Weekday, wakeUpTime: Date?) {
+//        self.init()
+//        self.weekday = weekday
+//        self.wakeUpTime = wakeUpTime
+//    }
+//
+//    //TODO: Docstrings
+//    func setWakeUpTime(wakeUpTime: Date) {
+//        self.wakeUpTime = wakeUpTime
+//        self.printDebug("Set wake up time for weekday value \(self.weekdayInt) to \(self.wakeUpTime)")
+//    }
+//
+//    override static func primaryKey() -> String? {
+//         return "_id"
+//    }
+//
+//    func printDebug(_ message: String) {
+//        if self.debug {
+//            print("$LOG (WakeUpTime): \(message)")
+//        }
+//    }
+//}
 
 extension UserSettings: Debuggable {
     func printDebug(_ message: String) {
