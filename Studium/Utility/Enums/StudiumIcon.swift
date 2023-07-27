f370b31d4be94d9b0a01bb55ecba322c193a6c49
Vikram Singh <vik.devmail@gmail.com>
2023-07-27 16:04:12 -0400

    NC: Implement improved LogoSection flow (sections with headers)

--- Studium/Utility/Enums/StudiumIcon.swift
+++ Studium/Utility/Enums/StudiumIcon.swift
@@ -54,6 +54,8 @@ enum ThirdPartyIcon: String, CaseIterable, CreatesUIImage {
 // TODO: Docstrings
 enum StudiumIcon: String, CaseIterable, CreatesUIImage {
     case atom
+    case baseball
+    case basketball
     case bath
     case bell
     case binoculars
@@ -64,10 +66,12 @@ enum StudiumIcon: String, CaseIterable, CreatesUIImage {
     case bugs
     case building
     case burger
+    case calculate
     case calculator
     case campground
     case canadianMapleLeaf
     case cannabis
+    case chart
     case checkmark
     case chessBishop
     case chessKing
@@ -80,8 +84,10 @@ enum StudiumIcon: String, CaseIterable, CreatesUIImage {
     case code
     case computer
     case creditCard
+    case cricket
     case database
     case display
+    case divide
     case dna
     case dog
     case dollarSign
@@ -95,7 +101,11 @@ enum StudiumIcon: String, CaseIterable, CreatesUIImage {
     case fireBurner
     case flaskVial
     case folder
+    case football
+    case function
+    case gameController
     case gavel
+    case golfCourse
     case hammer
     case heart
     case hippo
@@ -107,34 +117,44 @@ enum StudiumIcon: String, CaseIterable, CreatesUIImage {
     case leaf
     case map
     case martiniGlass
+    case microscope
     case minus
     case moneyBill
     case moneyCheckDollar
     case monument
     case networkWired
     case otter
+    case pencil
     case personHiking
+    case personKayaking
+    case personSkateboarding
     case personSkiingNordic
     case personSkiing
     case personSnowboarding
+    case personSurfing
+    case personSwimming
     case personWalkingLuggage
     case personWalking
+    case pingpong
     case pizzaSlice
     case plane
     case plus
     case rightFromBracket
     case robot
+    case running
     case server
     case ship
     case shower
     case snowplow
     case soap
+    case soccer
     case tree
     case tv
     case user
     case utensils
     case vial
     case wallet
+    case windmill
     
     
     // TODO: Docstrings
@@ -145,5 +165,56 @@ enum StudiumIcon: String, CaseIterable, CreatesUIImage {
             Log.e("tried to create UIImage from StudiumIcon rawValue \(self.rawValue) but failed.", logToCrashlytics: true)
             return .actions
         }
+    }
+}
+
+enum StudiumIconGroup: String, CaseIterable {
+    case science
+    case math
+    case games
+    case sports
+    case nature
+    case computer
+    case entertainment
+    case chores
+    case financial
+    case food
+    case transportation
+    case symbols
+    case other
+    
+    var icons: [StudiumIcon] {
+        switch self {
+        case .science:
+            return [.atom, .dna, .vial, .laptopMedical, .heart, .flaskVial, .boltLightning, .microscope, .windmill]
+        case .games:
+            return [.chess, .chessKing, .chessKnight, .chessBishop, .chessPawn, .chessQueen, .chessRook]
+        case .math:
+            return [.plus, .minus, .calculator, .calculate, .divide, .chart, .function]
+        case .sports:
+            return [.dumbbell, .football, .basketball, .baseball, .soccer, .pingpong, .cricket, .golfCourse, .personSwimming, .personWalking, .personSkiing, .personSkiingNordic, .personSnowboarding, .personHiking, .running, .personSkateboarding, .personKayaking, .personSurfing]
+        case .nature:
+            return [.tree, .personHiking, .leaf, .fireBurner, .cannabis, .canadianMapleLeaf, .campground, .dog, .otter, .hippo, .feather, .bugs, .bug, .boltLightning, .binoculars]
+        case .computer:
+            return [.server, .robot, .networkWired, .laptopCode, .laptopMedical, .file, .fileCode, .download, .display, .database, .computer, .code, .gameController]
+        case .entertainment:
+            return [.tv, .gameController, .martiniGlass, .cannabis, .book, .binoculars]
+        case .chores:
+            return [.soap, .snowplow, .shower, .kitchenSet, .hammer, .gavel, .dog, .bath, .pencil]
+        case .financial:
+            return [.wallet, .moneyBill, .moneyCheckDollar, .dollarSign, .creditCard, .chart]
+        case .food:
+            return [.utensils, .pizzaSlice, .martiniGlass, .kitchenSet, .burger, .blender]
+        case .transportation:
+            return [.ship, .ferry, .plane, .personWalkingLuggage, .map]
+        case .symbols:
+            return [.failureMark, .clock, .checkmark, .bell, .pencil]
+        case .other:
+            return [.user, .monument, .landmark, .house, .folder, .building, .rightFromBracket]
+        }
+    }
+    
+    var label: String {
+        return self.rawValue.capitalized
     }
 }
