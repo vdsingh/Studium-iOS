ad15467b9478f1f612227f0c0a24dc96aa6af018
Vikram Singh <vik.devmail@gmail.com>
2023-08-09 23:10:29 -0700

    NC: ColorPickerCellV2

--- Studium/Flows/CoursesFlow/Controllers/AddCourseViewController.swift
+++ Studium/Flows/CoursesFlow/Controllers/AddCourseViewController.swift
@@ -63,7 +63,11 @@ class AddCourseViewController: MasterForm, AlertTimeSelectingForm, LogoSelecting
             ],
             [
                 .logoCell(logo: self.icon.uiImage, onClick: { self.showLogoSelectionViewController() }),
-                .colorPickerCell(delegate: self),
+                .colorPickerCellV2(colorWasSelected: { color in
+                    print("SELECTED COLOR: \(color)")
+                    self.color = UIColor(color)
+                }),
+//                .colorPickerCell(delegate: self),
                 .textFieldCell(placeholderText: "Additional Details", text: self.additionalDetails, charLimit: TextFieldCharLimit.longField.rawValue, id: FormCellID.TextFieldCellID.additionalDetailsTextField, textFieldDelegate: self, delegate: self)
             ],
             [
