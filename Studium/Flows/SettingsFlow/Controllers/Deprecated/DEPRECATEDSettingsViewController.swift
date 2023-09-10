ce6e2a026c2b22f76047882df46e73fc3c0c7ac5
Vikram Singh <53489317+vdsingh@users.noreply.github.com>
2023-09-09 19:07:43 -0700

    Settings Revamp

---
+++ Studium/Flows/SettingsFlow/Controllers/Deprecated/DEPRECATEDSettingsViewController.swift
@@ -0,0 +1,238 @@
+//
+//  SettingsViewController.swift
+//  Studium
+//
+//  Created by Vikram Singh on 7/21/20.
+//  Copyright © 2020 Vikram Singh. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+import UIKit
+import EventKit
+import GoogleSignIn
+import TableViewFormKit
+
+import FirebaseCrashlytics
+
+// TODO: Docstrings
+class DEPRECATEDSettingsViewController: TableViewForm, Storyboarded, ErrorShowing, Coordinated {
+        
+    // TODO: Docstrings
+    private struct Constants {
+        static let deleteAllAssignmentsAlertInfo = (title: "Delete All Assignments", message: "Are you sure you want to delete all assignments? You can't undo this action.")
+        static let deleteCompletedAssignmentsAlertInfo = (title: "Delete Completed Assignments", message: "Are you sure you want to delete all completed assignments? You can't undo this action.")
+        static let deleteAllOtherEventsAlertInfo = (title: "Delete All Other Events", message: "Are you sure you want to delete all other events? You can't undo this action.")
+        static let deleteAllCompletedOtherEventsAlertInfo = (title: "Delete All Completed Other Events", message: "Are you sure you want to delete all completed other events? You can't undo this action.")
+    }
+    
+    // TODO: Docstrings
+    weak var coordinator: SettingsCoordinator?
+    
+    // TODO: Docstrings
+    var databaseService: DatabaseServiceProtocol! = DatabaseService.shared
+    
+    let studiumEventService: StudiumEventService = StudiumEventService.shared
+    
+    //TODO: Docstrings
+    lazy var cellData: [[(text: String, icon: (any CreatesUIImage)?, didSelect: (() -> Void)?)]] = [
+        
+        [
+            ("Sync with Apple Calendar", ThirdPartyIcon.appleCalendar, didSelect: {
+                AppleCalendarService.shared.syncCalendar{ _ in }
+            }),
+            ("Sync with Google Calendar", ThirdPartyIcon.googleCalendar, didSelect: {
+                GoogleCalendarService.shared.authenticateWithCalendarScope(
+                    presentingViewController: self
+                )
+            })
+        ],
+        [
+            ("Set Default Notifications", StudiumIcon.bell, didSelect: {
+                self.unwrapCoordinatorOrShowError()
+                self.coordinator?.showAlertTimesSelectionViewController(
+                    updateDelegate: self,
+                    selectedAlertOptions: self.databaseService.getDefaultAlertOptions(),
+                    viewControllerTitle: "Default Notifications"
+                )
+            }),
+            ("Reset Wake Up Times", StudiumIcon.clock, didSelect: {
+                self.unwrapCoordinatorOrShowError()
+                self.coordinator?.showUserSetupFlow()
+            })
+        ],
+        [
+            ("Delete All Assignments", nil, didSelect: {
+                self.createAlertForAssignments(title: Constants.deleteAllAssignmentsAlertInfo.title, message: Constants.deleteAllAssignmentsAlertInfo.message, isCompleted: false)
+            }),
+            ("Delete Completed Assignments", nil, didSelect: {
+                self.createAlertForAssignments(title: Constants.deleteCompletedAssignmentsAlertInfo.title, message: Constants.deleteCompletedAssignmentsAlertInfo.message, isCompleted: true)
+            }),
+            ("Delete All Other Events", nil, didSelect: {
+                self.createAlertForOtherEvents(title: Constants.deleteAllOtherEventsAlertInfo.title, message: Constants.deleteAllOtherEventsAlertInfo.message, isCompleted: false)
+            }),
+            ("Delete All Completed Other Events", nil, didSelect: {
+                self.createAlertForOtherEvents(title: Constants.deleteAllCompletedOtherEventsAlertInfo.title, message: Constants.deleteAllCompletedOtherEventsAlertInfo.message, isCompleted: true)
+                
+            })
+        ],
+        
+        [
+            ("Report a Problem", StudiumIcon.bug, didSelect: {
+                PopUpService.shared.showForm(formType: .reportAProblem) { textContents in
+                    if let email = textContents.first,
+                    let problemDetails = textContents.last {
+                        if problemDetails.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+                            PopUpService.shared.presentToast(title: "Error Reporting Problem", description: "Please specify the problem details.", popUpType: .failure)
+                        } else {
+                            PopUpService.shared.presentToast(title: "Message Sent", description: "Thanks for the feedback!", popUpType: .success)
+                            CrashlyticsService.shared.reportAProblem(email: email, message: problemDetails)
+                        }
+                    }
+                }
+            })
+        ],
+        [
+            ("ID: \(AuthenticationService.shared.userEmail ?? "Unavailable")", StudiumIcon.user, nil),
+            ("Sign Out", StudiumIcon.rightFromBracket, didSelect: {
+                AuthenticationService.shared.handleLogOut { error in
+                    if let error = error {
+                        Log.e(error)
+                        PopUpService.shared.presentToast(title: "Error Logging Out", description: "Try restarting the app.", popUpType: .failure)
+                        return
+                    }
+                    
+                    self.unwrapCoordinatorOrShowError()
+                    self.coordinator?.showAuthenticationFlow()
+                }
+            })
+        ]
+    ]
+    
+    func setCells() {
+        self.cells = cellData.map({ sectionData in
+            return sectionData.map { cellData in
+                return .labelCell(
+                    cellText: cellData.text,
+                    icon: cellData.icon?.uiImage,
+                    onClick: cellData.didSelect
+                )
+            }
+        })
+    }
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setCells()
+        self.tableView.tableFooterView = UIView()
+        
+        
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: StudiumColor.primaryLabel.uiColor]
+        self.navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: StudiumColor.primaryLabel.uiColor]
+        self.navigationController?.navigationBar.tintColor = StudiumColor.primaryAccent.uiColor
+        
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+        
+        self.view.backgroundColor = StudiumColor.background.uiColor
+        self.navigationController?.navigationBar.barTintColor = StudiumColor.background.uiColor
+    }
+    
+    //TODO: Docstrings
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return cellData.count
+    }
+    
+    //TODO: Docstrings
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellData[section].count
+    }
+    
+    //TODO: Docstrings
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+    //TODO: Docstrings
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if section == 0 {
+            return 0
+        }
+        
+        return Increment.two
+    }
+    
+    //TODO: Docstrings
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cellData = self.cellData[indexPath.section][indexPath.row]
+        
+        if let selectAction = cellData.didSelect {
+            selectAction()
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //edit the background color of section headers
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView?{
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.bounds.size.width, height: Increment.two))
+        headerView.backgroundColor = StudiumColor.background.uiColor
+        return headerView
+    }
+    
+    //TODO: Docstrings
+    private func deleteAllAssignments(isCompleted: Bool) {
+        let assignments = self.databaseService.getStudiumObjects(expecting: Assignment.self)
+        for assignment in assignments {
+            if (isCompleted && assignment.complete) || !isCompleted {
+                self.studiumEventService.deleteStudiumEvent(assignment)
+            }
+        }
+    }
+    
+    //TODO: Docstrings
+    private func deleteAllOtherEvents(isCompleted: Bool) {
+        let otherEvents = self.databaseService.getStudiumObjects(expecting: OtherEvent.self)
+        for event in otherEvents {
+            if (isCompleted && event.complete) || !isCompleted {
+                self.studiumEventService.deleteStudiumEvent(event)
+            }
+        }
+    }
+    
+    //TODO: Docstrings
+    private func createAlertForAssignments(title: String, message: String, isCompleted: Bool) {
+        let refreshAlert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        
+        refreshAlert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action: UIAlertAction!) in
+            self.deleteAllAssignments(isCompleted: isCompleted)
+        }))
+        
+        refreshAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (action: UIAlertAction!) in
+        }))
+        
+        present(refreshAlert, animated: true, completion: nil)
+    }
+    
+    //TODO: Docstrings
+    private func createAlertForOtherEvents(title: String, message: String, isCompleted: Bool){
+        let refreshAlert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        
+        refreshAlert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action: UIAlertAction!) in
+            self.deleteAllOtherEvents(isCompleted: isCompleted)
+        }))
+        
+        refreshAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (action: UIAlertAction!) in
+        }))
+        
+        present(refreshAlert, animated: true, completion: nil)
+    }
+}
+
+extension DEPRECATEDSettingsViewController: AlertTimeHandler {
+    // TODO: Docstrings
+    func alertTimesWereUpdated(selectedAlertOptions: [AlertOption]) {
+        self.databaseService.setDefaultAlertOptions(alertOptions: selectedAlertOptions)
+    }
+}
--- Studium/Flows/SettingsFlow/Controllers/SettingsViewController.swift
+++ Studium/Flows/SettingsFlow/Controllers/SettingsViewController.swift
@@ -1,34 +1,79 @@
 //
-//  SettingsViewController.swift
+//  NewSettingsViewController.swift
 //  Studium
 //
-//  Created by Vikram Singh on 7/21/20.
-//  Copyright © 2020 Vikram Singh. All rights reserved.
+//  Created by Vikram Singh on 9/9/23.
+//  Copyright © 2023 Vikram Singh. All rights reserved.
 //
 
 import Foundation
-import RealmSwift
+import SwiftUI
 import UIKit
-import EventKit
-import GoogleSignIn
-import TableViewFormKit
+import VikUtilityKit
 
-import FirebaseCrashlytics
-
-// TODO: Docstrings
-class SettingsViewController: TableViewForm, Storyboarded, ErrorShowing, Coordinated, Debuggable {
+struct SettingsCell: View {
     
-    let debug = true
+    private var icon: UIImage?
+    private var iconRenderingMode: Image.TemplateRenderingMode = .template
+    private var text: String
+    private var onClick: (SettingsCell) -> Void
+    @State var loading: Bool = false
     
-    // TODO: Docstrings
-    private struct Constants {
-        static let deleteAllAssignmentsAlertInfo = (title: "Delete All Assignments", message: "Are you sure you want to delete all assignments? You can't undo this action.")
-        static let deleteCompletedAssignmentsAlertInfo = (title: "Delete Completed Assignments", message: "Are you sure you want to delete all completed assignments? You can't undo this action.")
-        static let deleteAllOtherEventsAlertInfo = (title: "Delete All Other Events", message: "Are you sure you want to delete all other events? You can't undo this action.")
-        static let deleteAllCompletedOtherEventsAlertInfo = (title: "Delete All Completed Other Events", message: "Are you sure you want to delete all completed other events? You can't undo this action.")
+    var body: some View {
+        Button {
+            self.loading = true
+            self.onClick(self)
+        } label: {
+            if self.loading {
+                HStack(spacing: Increment.two) {
+                    Spacer()
+                    Spinner()
+                        .frame(height: Increment.six)
+                    Spacer()
+                }
+            } else {
+                HStack {
+                    if let icon = self.icon {
+                        SmallIcon(image: icon, renderingMode: self.iconRenderingMode)
+                    }
+                    
+                    StudiumText(self.text)
+                }
+            }
+        }
     }
     
-    // TODO: Docstrings
+    init(icon: UIImage,
+         text: String,
+         onClick: @escaping (SettingsCell) -> Void
+    ) {
+        self.icon = icon
+        self.text = text
+        self.onClick = onClick
+    }
+    
+    init(
+        icon: any CreatesUIImage,
+        iconRenderingMode: Image.TemplateRenderingMode = .template,
+        text: String,
+        onClick: @escaping (SettingsCell) -> Void
+    ) {
+        self.init(icon: icon.uiImage, text: text, onClick: onClick)
+        self.iconRenderingMode = iconRenderingMode
+    }
+    
+    init(text: String, onClick: @escaping (SettingsCell) -> Void) {
+        self.icon = nil
+        self.text = text
+        self.onClick = onClick
+    }
+}
+
+struct SettingsView: View {
+    let databaseService: DatabaseService = .shared
+    let studiumEventService: StudiumEventService = .shared
+    let presentingViewController: UIViewController
+    let alertTimeDelegate: AlertTimeHandler
     weak var coordinator: SettingsCoordinator?
     
     // TODO: Docstrings
@@ -90,11 +135,140 @@ class SettingsViewController: TableViewForm, Storyboarded, ErrorShowing, Coordin
                         Log.e(error)
                         PopUpService.shared.presentToast(title: "Error Logging Out", description: "Try restarting the app.", popUpType: .failure)
                         return
+
+    var body: some View {
+        NavigationView {
+            List {
+                Section("Calendar Sync") {
+                    SettingsCell(icon: ThirdPartyIcon.appleCalendar, iconRenderingMode: .original, text: "Sync with Apple Calendar") { cell in
+                        AppleCalendarService.shared.syncCalendar { _ in
+                            cell.loading = false
+                        }
                     }
                     
-                    self.unwrapCoordinatorOrShowError()
-                    self.coordinator?.showAuthenticationFlow()
+                    SettingsCell(icon: ThirdPartyIcon.googleCalendar, iconRenderingMode: .original, text: "Sync with Google Calendar") { cell in
+                        GoogleCalendarService.shared.authenticateWithCalendarScope(
+                            presentingViewController: self.presentingViewController
+                        )
+                        cell.loading = false
+                    }
                 }
+                
+                Section("Defaults") {
+                    SettingsCell(icon: StudiumIcon.bell, text: "Default Reminders Settings") { cell in
+                        cell.loading = false
+                        guard let coordinator = self.coordinator else {
+                            Log.e("Coordinator was nil")
+                            PopUpService.shared.presentGenericError()
+                            return
+                        }
+                        
+                        coordinator.showAlertTimesSelectionViewController(
+                            updateDelegate: self.alertTimeDelegate,
+                            selectedAlertOptions: self.databaseService.getDefaultAlertOptions(),
+                            viewControllerTitle: "Default Notifications"
+                        )
+                    }
+                    
+                    SettingsCell(icon: SystemIcon.sunMax.createImage(), text: "Reset Wake Up Times") { cell in
+                        cell.loading = false
+                        guard let coordinator = self.coordinator else {
+                            Log.e("Coordinator was nil")
+                            PopUpService.shared.presentGenericError()
+                            return
+                        }
+                        
+                        coordinator.showUserSetupFlow()
+                    }
+                }
+                
+                Section("Quick Delete") {
+                    SettingsCell(icon: SystemIcon.trashCan.createImage(), text: "Delete all Assignments") { cell in
+                        cell.loading = false
+                        let assignments = self.databaseService.getStudiumObjects(expecting: Assignment.self)
+                        for assignment in assignments {
+                                self.studiumEventService.deleteStudiumEvent(assignment)
+                        }
+                    }
+                    
+                    SettingsCell(icon: SystemIcon.trashCan.createImage(), text: "Delete Completed Assignments") { cell in
+                        cell.loading = false
+                        let assignments = self.databaseService.getStudiumObjects(expecting: Assignment.self)
+                        for assignment in assignments {
+                            if assignment.complete {
+                                self.studiumEventService.deleteStudiumEvent(assignment)
+                            }
+                        }
+                    }
+                    
+                    SettingsCell(icon: SystemIcon.trashCan.createImage(), text: "Delete all To-Do Events") { cell in
+                        cell.loading = false
+                        PopUpService.shared.presentDeleteAlert {
+                            let otherEvents = self.databaseService.getStudiumObjects(expecting: OtherEvent.self)
+                            for event in otherEvents {
+                                self.studiumEventService.deleteStudiumEvent(event)
+                            }
+                        }
+                    }
+                    
+                    SettingsCell(icon: SystemIcon.trashCan.createImage(), text: "Delete all Completed To-Do Events") { cell in
+                        cell.loading = false
+                        PopUpService.shared.presentDeleteAlert {
+                            let otherEvents = self.databaseService.getStudiumObjects(expecting: OtherEvent.self)
+                            for event in otherEvents {
+                                if event.complete {
+                                    self.studiumEventService.deleteStudiumEvent(event)
+                                }
+                            }
+                        }
+                    }
+                }
+                
+                Section("Feedback") {
+                    SettingsCell(icon: StudiumIcon.bug, text: "Report a Problem") { cell in
+                        cell.loading = false
+                        PopUpService.shared.showForm(formType: .reportAProblem) { textContents in
+                            if let email = textContents.first,
+                               let problemDetails = textContents.last {
+                                if problemDetails.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+                                    PopUpService.shared.presentToast(title: "Error Reporting Problem", description: "Please specify the problem details.", popUpType: .failure)
+                                } else {
+                                    PopUpService.shared.presentToast(title: "Message Sent", description: "Thanks for the feedback!", popUpType: .success)
+                                    CrashlyticsService.shared.reportAProblem(email: email, message: problemDetails)
+                                }
+                            }
+                        }
+                    }
+                }
+                
+                Section("Account") {
+                    SettingsCell(icon: StudiumIcon.user, text: "Email") { cell in
+                        cell.loading = false
+>>>>>>> Stashed changes
+                    }
+                    
+                    SettingsCell(text: "Sign Out") { cell in
+                        AuthenticationService.shared.handleLogOut { error in
+                            cell.loading = false
+                            if let error {
+                                Log.e(error)
+                                PopUpService.shared.presentToast(title: "Error Logging Out",
+                                                                 description: "Try restarting the app.",
+                                                                 popUpType: .failure)
+                                return
+                            }
+                            
+                            guard let coordinator = self.coordinator else {
+                                Log.e("Coordinator was nil")
+                                PopUpService.shared.presentGenericError()
+                                return
+                            }
+                            
+                            coordinator.showAuthenticationFlow()
+                        }
+                    }
+                }
+<<<<<<< Updated upstream
             })
             
         ]
@@ -109,75 +283,55 @@ class SettingsViewController: TableViewForm, Storyboarded, ErrorShowing, Coordin
                     icon: cellData.icon?.image,
                     onClick: cellData.didSelect
                 )
+=======
+>>>>>>> Stashed changes
             }
-        })
+            .environment(\.defaultMinListRowHeight, Increment.ten)
+            .navigationTitle("Settings")
+        }
     }
+}
 
+class SettingsViewController: UIViewController {
+    weak var coordinator: SettingsCoordinator?
+    let databaseService: DatabaseService
+
+    init(coordinator: SettingsCoordinator?, databaseService: DatabaseService) {
+        self.coordinator = coordinator
+        self.databaseService = databaseService
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.setCells()
-        self.tableView.tableFooterView = UIView()
-        
-        
-        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: StudiumColor.primaryLabel.uiColor]
-        self.navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: StudiumColor.primaryLabel.uiColor]
-        self.navigationController?.navigationBar.tintColor = StudiumColor.primaryAccent.uiColor
-        
-        self.navigationController?.navigationBar.prefersLargeTitles = true
-        
-        
-        self.view.backgroundColor = StudiumColor.background.uiColor
-        self.navigationController?.navigationBar.barTintColor = StudiumColor.background.uiColor
+        self.setupSwiftUI()
+        self.navigationController?.navigationBar.prefersLargeTitles = false
     }
     
-    //TODO: Docstrings
-//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
-//        cell?.backgroundColor = StudiumColor.secondaryBackground.uiColor
-//        cell?.textLabel?.textColor = StudiumColor.primaryLabel.uiColor
-//        cell?.textLabel?.text = cellData[indexPath.section][indexPath.row].text
-//        
-//        let id = AuthenticationService.shared.userID
-//        if cellData[indexPath.section][indexPath.row].text == "Email" {
-//            cell?.textLabel?.text = id
-//        }
-//        
-//        return cell!
-//    }
-    
-    //TODO: Docstrings
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return cellData.count
-    }
-    
-    //TODO: Docstrings
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cellData[section].count
-    }
-    
-    //TODO: Docstrings
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 60
-    }
-    
-    //TODO: Docstrings
-    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        if section == 0 {
-            return 0
-        }
+    private func setupSwiftUI() {
+        let settingsView = SettingsView(
+            presentingViewController: self,
+            alertTimeDelegate: self,
+            coordinator: self.coordinator
+        )
         
-        return K.emptyHeaderHeight
-    }
-    
-    //TODO: Docstrings
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let cellData = self.cellData[indexPath.section][indexPath.row]
+        let hostingController = UIHostingController(rootView: settingsView)
+        self.addChild(hostingController)
+        self.view.addSubview(hostingController.view)
+        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
         
-        if let selectAction = cellData.didSelect {
-            selectAction()
-        }
+        NSLayoutConstraint.activate([
+            hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
+            hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
         
+<<<<<<< Updated upstream
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
@@ -234,6 +388,9 @@ class SettingsViewController: TableViewForm, Storyboarded, ErrorShowing, Coordin
         }))
         
         present(refreshAlert, animated: true, completion: nil)
+=======
+        hostingController.didMove(toParent: self)
+>>>>>>> Stashed changes
     }
 }
 
@@ -243,3 +400,22 @@ extension SettingsViewController: AlertTimeHandler {
         self.databaseService.setDefaultAlertOptions(alertOptions: selectedAlertOptions)
     }
 }
+
+//struct ContentView_Previews: PreviewProvider {
+//    
+//    static var weekdays: Set<Weekday> {
+//        var set = Set<Weekday>()
+//        set.insert(.wednesday)
+//        set.insert(.monday)
+//        return set
+//    }
+//    
+//    static let mockAssignment = Assignment(name: "Homework 4", additionalDetails: "Additional Details", complete: true, startDate: Date(), endDate: Date()+100000, notificationAlertTimes: [], autoschedulingConfig: nil, parentCourse: Course(name: "CS 320", location: "Building A", additionalDetails: "Hello World", startDate: Date(), endDate: Date(), color: .green, icon: .atom, alertTimes: []))
+//    
+//    static var previews: some View {
+//        AssignmentView(
+//            assignment: mockAssignment,
+//            pdfUrlWasPressed: { _ in }
+//        )
+//    }
+//}
