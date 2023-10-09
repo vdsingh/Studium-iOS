5c53c00c09b2e2fad9694ed5c5b7364e608c6792
Vikram Singh <53489317+vdsingh@users.noreply.github.com>
2023-10-08 23:47:05 -0700

    Add remote config flags

--- Studium/Modules/Services/RemoteConfig/RemoteConfigService.swift
+++ Studium/Modules/Services/RemoteConfig/RemoteConfigService.swift
@@ -11,24 +11,42 @@ import FirebaseCore
 import FirebaseAnalytics
 import FirebaseRemoteConfig
 
-//if Flags.allowHabitAutoscheduling {
-//...
-//}
-
-enum Flags: String, CaseIterable {
-    // MARK: - Booleans
-    case allowHabitAutoscheduling
+/// Boolean flags from Remote Config
+enum ServerFlags: String, CaseIterable {
     
+    // MARK: - Autoscheduling Related
+    case allowHabitAutoscheduling
+    case allowAssignmentAutoscheduling
+    case enableHabitAutoschedulingByDefault
+    case enableAssignmentAutoschedulingByDefault
+    
+    
+    case allowGoogleCalendarSync
+    case allowAppleCalendarSync
+    
+    // MARK: - Computed Properties
+    
+    /// The value of the flag
     var value: Bool {
         let value = Self.retrieveValue(forKey: self.rawValue)
         Log.d("Retrieved flag \(self.rawValue) with value \(value.boolValue). The source was \(value.source)")
         return value.boolValue
     }
     
-    /// Whether the flag should be enabled by default
-    var enabledByDefault: Bool {
+    /// What the flag defaults to if it couldn't be retrieved from the server
+    private var fallbackValue: Bool {
         switch self {
         case .allowHabitAutoscheduling:
+            return true
+        case .allowAssignmentAutoscheduling:
+            return true
+        case .enableHabitAutoschedulingByDefault:
+            return false
+        case .enableAssignmentAutoschedulingByDefault:
+            return false
+        case .allowGoogleCalendarSync:
+            return true
+        case .allowAppleCalendarSync:
             return true
         }
     }
@@ -40,8 +58,8 @@ enum Flags: String, CaseIterable {
     
     fileprivate static var defaults: [String: NSObject] {
         var map = [String: NSObject]()
-        for flag in Flags.allCases {
-            map[flag.rawValue] = NSNumber(booleanLiteral: flag.enabledByDefault)
+        for flag in ServerFlags.allCases {
+            map[flag.rawValue] = NSNumber(booleanLiteral: flag.fallbackValue)
         }
         
         return map
@@ -52,18 +70,17 @@ class RemoteConfigService {
     static let shared = RemoteConfigService()
     var remoteConfig: RemoteConfig
     
-    
     private init() {
         self.remoteConfig = RemoteConfig.remoteConfig()
         let settings = RemoteConfigSettings()
         settings.minimumFetchInterval = 0
         self.remoteConfig.configSettings = settings
-        self.remoteConfig.setDefaults(Flags.defaults)
+        self.remoteConfig.setDefaults(ServerFlags.defaults)
     }
     
     func refresh(completion: @escaping () -> Void) {
-        self.remoteConfig.fetch { (status, error) -> Void in
-            if status == .success {
+        self.remoteConfig.fetchAndActivate() { (status, error) -> Void in
+            if status == .successFetchedFromRemote || status == .successUsingPreFetchedData {
                 Log.g("Remote Config fetched successfully")
             } else {
                 if let error {
